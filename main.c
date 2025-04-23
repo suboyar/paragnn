5e05807c1a45cb439120923ddaccc2b89353cc76
suleymanboyar <suleymanboyar02@gmail.com>
2025-04-23 15:26:57 +0200

    Load dataset

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 #include <zlib.h>
 
 #define NOB_IMPLEMENTATION
+#define NOB_STRIP_PREFIX
+// #define NOB_TEMP_CAPACITY (100*1024*1024)
 #include "nob.h"
 
 #define ERROR_PRINT(format, ...)                                        \
@@ -17,81 +19,122 @@
   } while(0)
 
 
-typedef struct {
-    uint64_t *items;            // need to flaot32 for x
-    size_t count;
-    size_t capacity;
-} da_t;
+#define IDX(i, j, width) ((i) * (width) + (j))
+#define IDX3(i, j, k, height, width) ((i) * (height) * (width) + (j) * (width) + (k))
 
 typedef struct {
-  uint32_t num_nodes;
-  uint32_t num_node_features;
-  da_t *node_year;
-  da_t edge_index;          // Graph connectivity in COO format with shape [2, num_edges]
-  da_t *x;                       // Node feature matrix with shape [num_nodes, num_node_features] (features)
-  da_t *y;                       // node-level targets of shape [num_nodes, *] or graph-level targets of shape [1, *] (labels)
-
-  // void edge_attr;               // Edge feature matrix with shape [num_edges, num_edge_features] (Not relevant in ogb-arxiv)
-  // void pos;                     // Node position matrix with shape [num_nodes, num_dimensions] (Not relevant in ogb-arxiv)  
-
-  long *items;
-  size_t count;
-  size_t capacity;
-} data_t;
+  size_t num_edges;
+  size_t num_nodes;
+  size_t num_node_features;
+  size_t num_label_classes;
+  uint64_t *node_year;
+  uint64_t *edge_index; // Graph connectivity in COO format with shape [2, num_edges]
+  double   *x; // Node feature matrix with shape [num_nodes, num_node_features] (features)
+  uint64_t *y; // node-level targets of shape [num_nodes, num_label_classes] or graph-level targets of shape [1, num_label_classes] (labels)
+} ogb_arxiv_t;
 
 #define CHUNK 0x1000 // 4kb window size
 
-// TODO: The read_gz function should return the entire string, which should then
-//       be parsed as either uint64 or float32 as necessary.
-
-void read_gz(char* file_path, da_t *da)
+String_View read_gz(char* file_path, String_Builder *sb)
 {
   gzFile file = gzopen(file_path, "rb");
   if (!file) { FATAL_ERROR("gzopen of '%s' failed: %s", file_path, strerror(errno)); }
 
-  Nob_String_Builder sb = {0};
-  char buffer[CHUNK];
-  int bytes_read;
-  do {
-    bytes_read = gzread(file, buffer, CHUNK);
-    // printf("bytes_read: %d\n", bytes_read);
-    if (bytes_read > 0) {
-      for (int i = 0; i < bytes_read; i++) {
-        if (buffer[i] != '\n') {
-          nob_da_append(&sb, buffer[i]);
-        } else {
-          nob_da_append(&sb, '\0');
-          nob_da_append(da, atoll(sb.items));
-          sb.count = 0;
-        }
-      }
-    }
-  } while (bytes_read > 0);
 
+  sb->count = 0;
+  char buffer[CHUNK];
+  while (1) {
+    int bytes_read = gzread(file, buffer, CHUNK);
+
+    if (bytes_read <= 0) {
+      break;
+    }
+
+    sb_append_buf(sb, buffer, bytes_read);
+  }
+
+  int err = 0;
+  const char * error_string;
   if (!gzeof(file)) {
-    const char * error_string;
-    int err = 0;
     error_string = gzerror (file, &err);
-    if (err) { FATAL_ERROR("gzread error: %s", error_string); }
   }
 
   gzclose(file);
 
+  if (err){
+    FATAL_ERROR("gzread of '%s' failed: %s", file_path, error_string);
+  }
+
+  sb_append_null(sb);
+  return sb_to_sv(*sb);
 }
+
+#define RAW_PATH "/home/sboyar/D1/dataset/ogb/data/ogbn_arxiv/raw"
 
 int main(void)
 {
-  char* raw_path = "/home/sboyar/D1/dataset/ogb/data/ogbn_arxiv/raw";
-  char label_path[1024];
-  strcpy(label_path, raw_path);
-  strcat(label_path, "/node-label.csv.gz");
-  
-  da_t labels = {0};z
-  read_gz(label_path, &labels);
-  nob_da_foreach(uint64_t, label, &labels) {
-    printf("%lu\n", *label);
+  ogb_arxiv_t arxiv = {0};
+  String_Builder sb = {0};
+  String_View sv = {0};
+
+  sv = read_gz(RAW_PATH"/num-node-list.csv.gz", &sb);
+  String_View num_node_sv = sv_chop_by_delim(&sv, '\n');
+  const char *num_node_cstr = temp_sv_to_cstr(num_node_sv);
+  arxiv.num_nodes = atoll(num_node_cstr);
+
+  sv = read_gz(RAW_PATH"/num-edge-list.csv.gz", &sb);
+  String_View num_edge_sv = sv_chop_by_delim(&sv, '\n');
+  const char *num_edge_cstr = temp_sv_to_cstr(num_edge_sv);
+  arxiv.num_edges = atoll(num_edge_cstr);
+
+  // TODO: Programatically find the number of labels through counting labels in
+  // the first line of ogbn_arxiv/raw/node-label.csv.gz
+  arxiv.num_label_classes = 1;
+  arxiv.y = malloc(arxiv.num_nodes * arxiv.num_label_classes * sizeof(*arxiv.y));
+  if (!arxiv.y) { FATAL_ERROR("Failed to allocate memory for labels"); }
+  sv = read_gz(RAW_PATH"/node-label.csv.gz", &sb);
+
+  for (size_t i = 0; sv.count > 0; i++) {
+    String_View labels_sv = sv_chop_by_delim(&sv, '\n');
+    // Jump over empty lines if any
+    if (labels_sv.data[labels_sv.count-1] == '\0') { continue; }
+
+    for (size_t j = 0; labels_sv.count > 0; j++) {
+      temp_reset();
+      String_View label_sv = sv_chop_by_delim(&labels_sv, ',');
+      const char *label_cstr = temp_sv_to_cstr(label_sv);
+      assert(strcmp(label_cstr, "") != 0);
+      arxiv.y[i] = atoll(label_cstr);
+      assert (i < arxiv.num_nodes);
+    }
   }
 
+  // TODO: Programatically find the number of features through counting features
+  // in the first line of ogbn_arxiv/raw/node-feat.csv.gz
+  arxiv.num_node_features = 128;
+  arxiv.x = malloc(arxiv.num_nodes * arxiv.num_node_features * sizeof(*arxiv.x));
+  if (!arxiv.x) { FATAL_ERROR("Failed to allocate memory for features"); }
+  sv = read_gz(RAW_PATH"/node-feat.csv.gz", &sb);
+
+  for (size_t i = 0; sv.count > 0; i++) {
+    String_View feats_sv = sv_chop_by_delim(&sv, '\n');
+    // Jump over empty lines if any
+    if (feats_sv.data[feats_sv.count-1] == '\0') { continue; }
+
+    for (size_t j = 0; feats_sv.count > 0; j++) {
+      temp_reset();
+      String_View feat_sv = sv_chop_by_delim(&feats_sv, ',');
+      const char *feat_cstr = temp_sv_to_cstr(feat_sv);
+      assert(strcmp(feat_cstr, "") != 0);
+      arxiv.x[IDX(i, j, arxiv.num_node_features)] = atof(feat_cstr);
+      assert(j < arxiv.num_node_features);
+    }
+    assert(i < arxiv.num_nodes);
+  }
+
+  free(arxiv.x);
+  free(arxiv.y);
+  sb_free(sb);
   return 0;
 }
 
