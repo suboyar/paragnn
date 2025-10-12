a526335b47f1570bb0fd327667b83b2c05d7bf78
suleymanboyar <suleymanboyar02@gmail.com>
2025-10-12 02:52:25 +0200

    Use flag.h

--- nob.c
+++ nob.c
@@ -1,40 +1,55 @@
-#define nob_cc_flags(cmd) nob_cmd_append(cmd, "-std=c17", "-D_POSIX_C_SOURCE=200809L")
-#define nob_cc_error_flags(cmd) nob_cmd_append(cmd, "-Wall", \
-                                                    "-Wextra", \
-                                                    "-Wfloat-conversion", \
-                                                    "-Werror=implicit-function-declaration", \
-                                                    "-Werror=incompatible-pointer-types")
 #define NOB_IMPLEMENTATION
 #define NOB_WARN_DEPRECATED
+#define nob_cc(cmd) nob_cmd_append(cmd, "gcc")
 #include "nob.h"
+#undef nob_cc_flags
+
+#define FLAG_IMPLEMENTATION
+#define FLAG_PUSH_DASH_DASH_BACK
+#include "flag.h"
 
 #define BUILD_FOLDER "build/"
 #define TOOLS_FOLDER "tools/"
 #define SRC_FOLDER "src/"
 
 #define NOB_NEEDS_REBUILD(output_path, input_paths, input_paths_count) \
-    (flags.force_rebuild ? 1 : nob_needs_rebuild((output_path), (input_paths), (input_paths_count)))
+    (flags.rebuild ? 1 : nob_needs_rebuild((output_path), (input_paths), (input_paths_count)))
 
 #define NOB_NEEDS_REBUILD1(output_path, input_path) \
-    (flags.force_rebuild ? 1 : nob_needs_rebuild1((output_path), (input_path)))
+    (flags.rebuild ? 1 : nob_needs_rebuild1((output_path), (input_path)))
 
-static struct {
-    bool        force_rebuild;
-    bool        build_ogb;
-    bool        release;
-    bool        compile_sbatch;
-    bool        run_sbatch;
-    const char* partition;
-    const char* out_dir;
-} flags = {
-    .force_rebuild  = false,
-    .build_ogb      = false,
-    .release        = false,
-    .compile_sbatch = false,
-    .run_sbatch     = false,
-    .partition      = NULL,
-    .out_dir        = NULL,
-};
+#define nob_cc_flags(cmd) nob_cmd_append(cmd, "-std=c17", "-D_POSIX_C_SOURCE=200809L")
+#define nob_cc_error_flags(cmd) \
+    nob_cmd_append(cmd,                                                 \
+                   "-Wall",                                             \
+                   "-Wextra",                                           \
+                   "-Wfloat-conversion",                                \
+                   "-Werror=implicit-function-declaration",             \
+                   "-Werror=incompatible-pointer-types")
+
+typedef struct {
+    bool  build;
+    bool  run;
+    bool  rebuild;
+    bool  build_ogb;
+    bool  release;
+    bool  submit;
+    char* outdir;
+    char* partition;
+    char* variant;
+} Flags;
+
+Flags flags = {0};
+
+typedef enum {
+    CMD_NONE,
+    CMD_CLEAN,
+    CMD_OGB,
+    CMD_COMPILE_LOCAL,
+    CMD_COMPILE_SLURM,
+    CMD_RUN_LOCAL,
+    CMD_RUN_SLURM,
+} Command;
 
 Nob_Cmd cmd = {0};
 Nob_Procs procs = {0};
@@ -44,11 +59,14 @@ struct {
     const char* arch;
     bool testing;
 } partitions[] = {
+    // For benchmarking
     {.name = "defq",     .arch = "arm",   .testing = false},
-    {.name = "genoaxq",  .arch = "amd",   .testing = false},
-    {.name = "milanq",   .arch = "amd",   .testing = false},
     {.name = "fpgaq",    .arch = "amd",   .testing = false},
+    {.name = "genoaxq",  .arch = "amd",   .testing = false},
+    {.name = "gh200q",   .arch = "arm",   .testing = false},
+    {.name = "milanq",   .arch = "amd",   .testing = false},
     {.name = "xeonmaxq", .arch = "intel", .testing = false},
+    // For testing
     {.name = "rome16q",  .arch = "amd",   .testing = true},
 };
 
@@ -88,19 +106,51 @@ bool partition_is_valid(const char* partition)
     return false;
 }
 
-int build_paragnn(const char* exec, const char* out_dir)
+
+char* to_define_flag(const char* str)
 {
+    Nob_String_Builder sb = {0};
+    const char* p = str;
+
+    while (*p) {
+        nob_da_append(&sb, (char)toupper((unsigned char) *p++));
+    }
+    nob_sb_append_null(&sb);
+
+    return nob_temp_sprintf("-D%s", sb.items);
+}
+
+int build_paragnn()
+{
+    const char *exec = "paragnn";
+    char *out_dir = BUILD_FOLDER;
+    if (!nob_mkdir_if_not_exists(out_dir)) return 1;
+
+    if (flags.variant != NULL){
+        out_dir = nob_temp_sprintf("%s%s/", out_dir, flags.variant);
+        if (!nob_mkdir_if_not_exists(out_dir)) return 1;
+    }
+
+    if (flags.partition != NULL){
+        out_dir = nob_temp_sprintf("%s%s/", out_dir, flags.partition);
+        if (!nob_mkdir_if_not_exists(out_dir)) return 1;
+    }
+
     const char* exec_path = nob_temp_sprintf("%s%s", out_dir, exec);
+    const char* variant_path = SRC_FOLDER;
+    if (flags.variant != NULL) {
+        variant_path = nob_temp_sprintf("%s%s/", variant_path, flags.variant);
+    }
     struct {
         const char* obj_path;
         const char* src_path;
     } targets[] = {
-        {.obj_path = (const char*)(nob_temp_sprintf("%s%s", out_dir, "gnn.o")),       .src_path = SRC_FOLDER"gnn.c"},
-        {.obj_path = (const char*)(nob_temp_sprintf("%s%s", out_dir, "graph.o")),     .src_path = SRC_FOLDER"graph.c"},
-        {.obj_path = (const char*)(nob_temp_sprintf("%s%s", out_dir, "layers.o")),    .src_path = SRC_FOLDER"layers.c"},
-        {.obj_path = (const char*)(nob_temp_sprintf("%s%s", out_dir, "main.o")),      .src_path = SRC_FOLDER"main.c"},
-        {.obj_path = (const char*)(nob_temp_sprintf("%s%s", out_dir, "matrix.o")),    .src_path = SRC_FOLDER"matrix.c"},
-        {.obj_path = (const char*)(nob_temp_sprintf("%s%s", out_dir, "perf.o")), .src_path = SRC_FOLDER"perf.c"},
+        {.obj_path = (nob_temp_sprintf("%s%s", out_dir, "gnn.o")),    .src_path = nob_temp_sprintf("%s%s", variant_path, "gnn.c")},
+        {.obj_path = (nob_temp_sprintf("%s%s", out_dir, "graph.o")),  .src_path = SRC_FOLDER"graph.c"},
+        {.obj_path = (nob_temp_sprintf("%s%s", out_dir, "layers.o")), .src_path = nob_temp_sprintf("%s%s", variant_path, "layers.c")},
+        {.obj_path = (nob_temp_sprintf("%s%s", out_dir, "main.o")),   .src_path = SRC_FOLDER"main.c"},
+        {.obj_path = (nob_temp_sprintf("%s%s", out_dir, "matrix.o")), .src_path = nob_temp_sprintf("%s%s", variant_path, "matrix.c")},
+        {.obj_path = (nob_temp_sprintf("%s%s", out_dir, "perf.o")),   .src_path = SRC_FOLDER"perf.c"},
     };
 
     // Compile src files
@@ -110,12 +160,23 @@ int build_paragnn(const char* exec, const char* out_dir)
             nob_cc_flags(&cmd);
             nob_cc_error_flags(&cmd);
             nob_cmd_append(&cmd, "-I.");
+	        nob_cmd_append(&cmd, "-I"SRC_FOLDER);
+	        nob_cmd_append(&cmd, nob_temp_sprintf("-I%s", variant_path));
+
+            if (flags.variant != NULL) {
+                if (strcmp(flags.variant, "baseline") == 0) {
+                    // Default optimizations
+                    nob_cmd_append(&cmd, "-DBASELINE");
+                }
+            }
+
             if (flags.release) {
                 nob_cmd_append(&cmd, "-O3", "-march=native", "-DNDEBUG");
                 nob_cmd_append(&cmd, "-DUSE_OGB_ARXIV");
             } else {
                 nob_cmd_append(&cmd, "-O0", "-ggdb", "-g3", "-gdwarf-2");
             }
+
             nob_cc_output(&cmd, targets[i].obj_path);
             nob_cmd_append(&cmd, "-c", targets[i].src_path);
             if (!nob_cmd_run(&cmd, .async = &procs)) return 1;
@@ -152,9 +213,9 @@ int build_paragnn(const char* exec, const char* out_dir)
 }
 
 
-int build_ogb(const char* exec, const char* out_dir){
-    const char* exec_path = nob_temp_sprintf("%s%s", out_dir, exec);
-    const char* obj_path = nob_temp_sprintf("%s%s", out_dir, "ogb.o");
+int build_ogb(){
+    const char* exec = TOOLS_FOLDER"ogb";
+    const char* obj_path = TOOLS_FOLDER"ogb.o";
     const char* src_path = TOOLS_FOLDER"ogb.c";
 
     // Compile src files
@@ -175,7 +236,7 @@ int build_ogb(const char* exec, const char* out_dir){
         nob_cc_flags(&cmd);
         nob_cc_error_flags(&cmd);
         nob_cmd_append(&cmd, "-O3");
-        nob_cc_output(&cmd, exec_path);
+        nob_cc_output(&cmd, exec);
         nob_cc_inputs(&cmd, obj_path);
         nob_cmd_append(&cmd, "-lz");
         if (!nob_cmd_run(&cmd)) return 1;
@@ -192,122 +253,65 @@ int clean()
     return 0;
 }
 
-int sbatch(const char* partition)
+int submit_job()
 {
-    nob_cmd_append(&cmd, "sbatch");
-    nob_cmd_append(&cmd, "-p", partition);
-    if (flags.run_sbatch) nob_cmd_append(&cmd, "run.sbatch");
-    else nob_cmd_append(&cmd, "compile.sbatch");
+    if (flags.build) {
+        if (!nob_mkdir_if_not_exists("./logs/compile")) return 1;
 
-    if (!nob_cmd_run(&cmd)) return 1;
+        nob_cmd_append(&cmd, "sbatch");
+        nob_cmd_append(&cmd, "-p", flags.partition);
+        nob_cmd_append(&cmd, "--export", nob_temp_sprintf("ALL,EXPERIMENT=%s", flags.variant));
+        nob_cmd_append(&cmd, "ex3/compile.sbatch");
+        if (!nob_cmd_run(&cmd)) return 1;
+    } else if (flags.run) {
+        nob_cmd_append(&cmd, "sbatch");
+        nob_cmd_append(&cmd, "-p", flags.partition);
+        nob_cmd_append(&cmd, "--export", nob_temp_sprintf("ALL,EXPERIMENT=%s", flags.variant));
+        nob_cmd_append(&cmd, nob_temp_sprintf("%s-run.sbatch", flags.partition));
+    } else {
+        nob_log(NOB_ERROR, "No job valid job");
+    }
 
     return 0;
 }
+
+void usage(FILE *stream)
+{
+    fprintf(stream, "Usage: ./example [OPTIONS] [--] [ARGS]\n");
+    fprintf(stream, "OPTIONS:\n");
+    flag_print_options(stream);
+}
+
 
 int main(int argc, char** argv)
 {
     NOB_GO_REBUILD_URSELF(argc, argv);
 
-    // Parse flags
-    nob_shift(argv, argc);      // jump over executable name in argv[0]
-    while (argc) {
-        const char* arg = nob_shift(argv, argc);
-        if (strcmp(arg, "--clean") == 0) {
-            return clean();
-        } else if (strcmp(arg, "--compile-sbatch") == 0) {
-            if (argc == 0) {
-                list_all_partitions();
-                fprintf(stderr, "Error: --compile-sbatch requires an argument, it should be one of: [list,all,partition name...]\n");
-                return 1;
-            }
+    flag_bool_var(&flags.build,         "build",     false,      "Build the project");
+    flag_bool_var(&flags.run,           "run",       false,      "Run the project");
+    flag_bool_var(&flags.rebuild,       "rebuild",   false,      "Force a complete rebuild");
+    flag_bool_var(&flags.build_ogb,     "build-ogb", false,      "Build OGB decoder");
+    flag_bool_var(&flags.release,       "release",   false,      "Build in release mode");
+    flag_bool_var(&flags.submit,        "submit",    false,      "Submit build job to SLURM instead of building locally");
+    flag_str_var(&flags.partition,      "partition", NULL,       "SLURM partition to use");
+    flag_str_var(&flags.variant,        "variant",   NULL, "Build variant (baseline, etc.)");
 
-            if (flags.run_sbatch) {
-                fprintf(stderr, "Error: --compile-sbatch can not be called together with --run-sbatch\n");
-                return 1;
-            }
-            flags.compile_sbatch = true;
-            flags.partition = nob_shift(argv, argc);
-            if (strcmp(flags.partition, "list") == 0) {
-                list_all_partitions();
-                return 0;
-            } else if (strcmp(flags.partition, "all") != 0 && !partition_is_valid(flags.partition)) {
-                list_all_partitions();
-                fprintf(stderr,
-                        "Error: --compile-sbatch received invalid argument '%s', it should be one of: [list,all,partition name...]\n",
-                        flags.partition);
-                return 1;
-            }
-        } else if (strcmp(arg, "--run-sbatch") == 0) {
-            if (argc == 0) {
-                list_all_partitions();
-                fprintf(stderr, "Error: --run-sbatch requires an argument, it should be one of: [list,all,partition name...]\n");
-                return 1;
-            }
-
-            if (flags.compile_sbatch) {
-                fprintf(stderr, "Error: --compile-run can not be called together with --compile-sbatch\n");
-                return 1;
-            }
-            flags.run_sbatch = true;
-            flags.partition = nob_shift(argv, argc);
-            if (strcmp(flags.partition, "list") == 0) {
-                list_all_partitions();
-                return 0;
-            } else if (strcmp(flags.partition, "all") != 0 && !partition_is_valid(flags.partition)) {
-                list_all_partitions();
-                fprintf(stderr,
-                        "Error: --run-sbatch received invalid argument '%s', it should be one of: [list,all,partition name...]\n",
-                        flags.partition);
-                return 1;
-            }
-        } else if (strcmp(arg, "--out-dir") == 0) {
-            if (argc == 0) {
-                fprintf(stderr, "Error: --out-dir requires an argument\n");
-                return 1;
-            }
-
-            flags.out_dir = nob_shift(argv, argc);
-        }
-
-        else if (strcmp(arg, "--rebuild") == 0) {
-            flags.force_rebuild = true;
-        } else if (strcmp(arg, "--ogb") == 0) {
-            flags.build_ogb = true;
-        } else if (strcmp(arg, "--release") == 0) {
-            flags.release = true;
-        } else {
-            fprintf(stderr, "Got invalid arg: %s\n", arg);
-            abort();
-        }
+    if (!flag_parse(argc, argv)) {
+        usage(stderr);
+        flag_print_error(stderr);
+        exit(1);
     }
 
     if (flags.build_ogb) {
-        char* out_dir = TOOLS_FOLDER;
-        char* exec_path = "ogb";
-        return build_ogb(exec_path, out_dir);
+        return build_ogb();
     }
 
-    else if (flags.compile_sbatch || flags.run_sbatch) {
-        if (strcmp(flags.partition, "all") == 0) {
-            for (size_t i = 0; i < NOB_ARRAY_LEN(partitions); i++) {
-                if (sbatch(partitions[i].name)) return 1;
-            }
-            return 0;
-        }
-        else return sbatch(flags.partition);
+    else if (flags.submit) {
+        return submit_job();
     }
-    else {
-        char* out_dir = BUILD_FOLDER;
-        char* exec = "paragnn";
 
-        if (flags.out_dir != NULL) {
-            out_dir = nob_temp_sprintf("%s%s/", out_dir, flags.out_dir);;
-        }
-        printf("out_dir: %s\n", out_dir);
-
-        if (!nob_mkdir_if_not_exists(out_dir)) return 1;
-
-        return build_paragnn(exec, out_dir);
+    if (flags.build || flags.rebuild) {
+        return build_paragnn();
     }
 
     return 0;
