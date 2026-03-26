c4c54d92a1003f913e10375c0a24243f04becd04
suleymanboyar <suleymanboyar02@gmail.com>
2026-03-27 00:08:04 +0100

    BREAKING: forgot to add this file

--- dataset_info.h
+++ dataset_info.h
@@ -1,6 +1,8 @@
 #ifndef DATASET_INFO_H
 #define DATASET_INFO_H
 
+#include <stdbool.h>
+
 typedef enum { FMT_CSV_GZ, FMT_NPY } RawFormat;
 
 typedef enum {
@@ -13,44 +15,52 @@ typedef enum {
 typedef struct {
     const char *name;
     const char *url;
+    const char *dir_name; // folder name inside the zip
+    const char *split_name; // name of the folder withing split/ folder
     uint32_t    num_nodes;
     uint32_t    num_features;
     uint32_t    num_classes;
     uint32_t    num_edges;
-    bool        undirected;     // if true num_edges needs to be recomputed at runtime
+    bool        undirected;
     RawFormat   raw_format;
 } DatasetInfo;
 
 DatasetInfo ds_infos[] = {
     [DATASET_ARXIV] = {
-        .name                = "arxiv",
-        .url                 = "https://snap.stanford.edu/ogb/data/nodeproppred/arxiv.zip",
-        .num_nodes           = 169343,
-        .num_features        = 128,
-        .num_classes         = 40,
-        .num_edges           = 1'166'243,
-        .undirected          = false,
-        .raw_format          = FMT_CSV_GZ,
+        .name         = "arxiv",
+        .url          = "https://snap.stanford.edu/ogb/data/nodeproppred/arxiv.zip",
+        .dir_name     = "arxiv",
+        .split_name   = "time",
+        .num_nodes    = 169343,
+        .num_features = 128,
+        .num_classes  = 40,
+        .num_edges    = 1166243,
+        .undirected   = false,
+        .raw_format   = FMT_CSV_GZ,
     },
     [DATASET_PRODUCTS] = {
-        .name                = "products",
-        .url                 = "https://snap.stanford.edu/ogb/data/nodeproppred/products.zip",
-        .num_nodes           = 2'449'029,
-        .num_features        = 100,
-        .num_classes         = 47,
-        .num_edges           = 61'859'140, // edge count is doubled sisnce inlcudes reverser loops it being undirected
-        .undirected          = true,
-        .raw_format          = FMT_CSV_GZ,
+        .name         = "products",
+        .url          = "https://snap.stanford.edu/ogb/data/nodeproppred/products.zip",
+        .dir_name     = "products",
+        .split_name   = "sales_ranking",
+        .num_nodes    = 2449029,
+        .num_features = 100,
+        .num_classes  = 47,
+        .num_edges    = 61859140, // edge count is doubled sisnce inlcudes reverser loops it being undirected
+        .undirected   = true,
+        .raw_format   = FMT_CSV_GZ,
     },
     [DATASET_PAPERS100M] = {
-        .name                = "papers100M",
-        .url                 = "https://snap.stanford.edu/ogb/data/nodeproppred/papers100M-bin.zip",
-        .num_nodes           = 111'059'956,
-        .num_features        = 128,
-        .num_classes         = 172,
-        .num_edges           = 1'615'685'872,
-        .undirected          = false,
-        .raw_format          = FMT_NPY,
+        .name         = "papers100M",
+        .url          = "https://snap.stanford.edu/ogb/data/nodeproppred/papers100M-bin.zip",
+        .dir_name     = "papers100M-bin",
+        .split_name   = "time",
+        .num_nodes    = 111059956,
+        .num_features = 128,
+        .num_classes  = 172,
+        .num_edges    = 1615685872,
+        .undirected   = false,
+        .raw_format   = FMT_NPY,
     }
 };
 
@@ -65,15 +75,6 @@ static inline DatasetKind str_to_dataset_kind(const char *str)
         return DATASET_PAPERS100M;
 
     return DATASET_INVALID;
-}
-
-static inline void list_datasets(void)
-{
-    printf("Available datasets:\n");
-    for (size_t i = 0; i < NOB_ARRAY_LEN(ds_infos); i++)
-    {
-        printf("  %s\n", ds_infos[i].name);
-    }
 }
 
 #endif // DATASET_INFO_H
